e324d4df65aa59e0bf53f88b08bc64eabb4777ed
harish <harish121192@gmail.com>
2019-09-20 21:00:02 +0530

    first commit

---
+++ src/test/java/qaclickacdemy/AppiumTest.java
@@ -0,0 +1,24 @@
+package qaclickacdemy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+
+	@Test
+	
+	public void AppiummTest()
+	{
+		System.out.println("NativeAppMethod");
+		
+	}
+	
+	@Test 
+	
+	public void iOSAndroid()
+	{
+		System.out.println("iOSAndroid");
+	}
+	
+}
